cbbdb4829b340eaab0bf8fe425f465b2da428f5d
weihanli <weihanli@outlook.com>
2020-11-27 00:28:06 +0800

    export prometheus metrics

--- SparkTodo.API/Startup.cs
+++ SparkTodo.API/Startup.cs
@@ -13,6 +13,7 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
+using Prometheus;
 using SparkTodo.API.Services;
 using SparkTodo.API.Swagger;
 using SparkTodo.DataAccess;
@@ -196,11 +197,15 @@ namespace SparkTodo.API
             });
 
             app.UseRouting();
+
+            app.UseHttpMetrics();
+
             app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapMetrics();
                 endpoints.MapControllers();
             });
         }
