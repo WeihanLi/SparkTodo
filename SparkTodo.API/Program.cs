abe68462c7c7c49c8e772ec7a33587f2ef830dc8
weihanli <weihanli@outlook.com>
2021-01-03 16:24:16 +0800

    use console json formatter

--- SparkTodo.API/Program.cs
+++ SparkTodo.API/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Prometheus.DotNetRuntime;
 using SparkTodo.Models;
 
@@ -21,11 +22,15 @@ namespace SparkTodo.API
                 .WithGcStats()
                 .WithThreadPoolStats()
                 .StartCollecting();
-            
+
             var host = Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webHostBuilder =>
                 {
                     webHostBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(loggingBuilder =>
+                {
+                    loggingBuilder.AddJsonConsole();
                 })
                 .Build();
 
