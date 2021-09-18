3880008478d7e7f14f88d3d758d1d5bf9fe11718
weihanli <weihanli@outlook.com>
2021-09-18 20:09:23 +0800

    Update Program.cs

--- SparkTodo.API/Program.cs
+++ SparkTodo.API/Program.cs
@@ -1,58 +1,45 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Prometheus.DotNetRuntime;
+using SparkTodo.API;
 using SparkTodo.Models;
 
-namespace SparkTodo.API
-{
-    public class Program
+DotNetRuntimeStatsBuilder.Customize()
+    .WithContentionStats()
+    .WithGcStats()
+    .WithThreadPoolStats()
+    .StartCollecting();
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webHostBuilder =>
     {
-        /// <summary>
-        /// program entry
-        /// </summary>
-        /// <param name="args">arguments</param>
-        public static async Task Main(string[] args)
+        webHostBuilder.UseStartup<Startup>();
+    })
+    .ConfigureLogging(loggingBuilder =>
+    {
+        loggingBuilder.AddJsonConsole();
+    })
+    .Build();
+
+using (var serviceScope = host.Services.CreateScope())
+{
+    var dbContext = serviceScope.ServiceProvider.GetRequiredService<SparkTodoDbContext>();
+    await dbContext.Database.EnsureCreatedAsync();
+
+    //init Database,you can add your init data here
+    var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<UserAccount>>();
+    var email = "weihanli@outlook.com";
+    if (await userManager.FindByEmailAsync(email) == null)
+    {
+        await userManager.CreateAsync(new UserAccount
         {
-            DotNetRuntimeStatsBuilder.Customize()
-                .WithContentionStats()
-                .WithGcStats()
-                .WithThreadPoolStats()
-                .StartCollecting();
-
-            var host = Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webHostBuilder =>
-                {
-                    webHostBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging(loggingBuilder =>
-                {
-                    loggingBuilder.AddJsonConsole();
-                })
-                .Build();
-
-            using (var serviceScope = host.Services.CreateScope())
-            {
-                var dbContext = serviceScope.ServiceProvider.GetRequiredService<SparkTodoDbContext>();
-                await dbContext.Database.EnsureCreatedAsync();
-
-                //init Database,you can add your init data here
-                var userManager = serviceScope.ServiceProvider.GetRequiredService<UserManager<UserAccount>>();
-                var email = "weihanli@outlook.com";
-                if (await userManager.FindByEmailAsync(email) == null)
-                {
-                    await userManager.CreateAsync(new UserAccount
-                    {
-                        UserName = email,
-                        Email = email
-                    }, "Test1234");
-                }
-            }
-
-            await host.RunAsync();
-        }
+            UserName = email,
+            Email = email
+        }, "Test1234");
     }
 }
+
+await host.RunAsync();
