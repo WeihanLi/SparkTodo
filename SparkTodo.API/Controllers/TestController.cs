77ebc6a620f89d235e72519732d6d9d6280334ad
Weihan Li <weihanli@outlook.com>
2024-11-13 02:11:27 +0800

    update Test

--- SparkTodo.API/Controllers/TestController.cs
+++ SparkTodo.API/Controllers/TestController.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Weihan Li. All rights reserved.
 // Licensed under the MIT license.
 
+using System.Diagnostics.Metrics;
 using WeihanLi.Common.Models;
 
 namespace SparkTodo.API.Controllers;
@@ -19,6 +20,18 @@ public class TestController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> ExternalApiTest()
     {
+        {
+            var meter = new Meter("test");
+            var counter = meter.CreateCounter<int>("counter");
+            counter.Add(1);
+        }
+
+        {
+            var meter = new Meter("test1");
+            var counter = meter.CreateCounter<int>("counter");
+            counter.Add(1);
+        }
+        
         var responseText = await HttpHelper.HttpClient.GetStringAsync("https://reservation.weihanli.xyz/health");
         return Ok(Result.Success(responseText));
     }
